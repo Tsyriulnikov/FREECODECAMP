22b42b84e2983d9a2c40546cb9dee7378a0a5055
Tsyriulnikov <max_djvu@mail.ru>
2022-02-16 21:01:10 +0300

    7 day

---
+++ fcc109.js
@@ -0,0 +1,5 @@
+function convertToInteger(str) {
+    return parseInt(str,2);//return integer по основанию 2 (в двоичной -> десятичную )
+    }
+    
+    convertToInteger("10011");
---
+++ fcc113.js
@@ -0,0 +1,13 @@
+function countup(n) {
+    if (n < 1) {
+      return [];
+    } else {
+      const countArray = countup(n - 1);
+      countArray.push(n);
+      return countArray;
+    }
+  }
+  console.log(countup(5));
+  //The value [1, 2, 3, 4, 5] will be displayed in the console.
+
+  
